3cfec3a5139e6df91f6a0784bced14d77bf7d174
unknown <watercolours@qq.com>
2018-12-03 17:51:59 +0800

    add header

--- Travel/src/pages/home/Home.vue
+++ Travel/src/pages/home/Home.vue
@@ -1,12 +1,19 @@
 <template>
     <div>
-         helloworld
+         <home-header></home-header>
     </div>
+
 </template>
 
 <script type="text/javascript">
+// 局部组件
+import HomeHeader from './components/Header.vue'
 export default{
-    name: 'Home'
+    name: 'Home',
+    components:{
+        //键和值一样的话就可以简写HomeHeader:HomeHeader => HomeHeader
+        HomeHeader
+    }
 }
 </script>
 <style>
