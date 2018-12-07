d88469551e2345011e10614cc040fd4c5e54c670
unknown <watercolours@qq.com>
2018-12-07 15:55:44 +0800

    finish city css

---
+++ Travel/src/pages/city/components/Alphabet.vue
@@ -0,0 +1,33 @@
+<template>
+        <ul class="list">
+            <li class="item">a</li>
+            <li class="item">b</li>
+            <li class="item">c</li>
+            <li class="item">d</li>
+        </ul>
+</template>
+
+<script type="text/javascript">
+
+export default{
+     name: 'CityAlphabet'
+}
+</script>
+<style lang="stylus" scoped>
+@import '~styles/varibles.styl'
+    .list
+        //做垂直居中用flex下面三条
+        display: flex
+        flex-direction: column
+        justify-content: center
+
+        position: absolute
+        top: 1.58rem
+        right: 0
+        width: .4rem
+        bottom:0
+        .item
+            line-height: .4rem
+            text-align: center
+            color: $bgColor
+</style>
