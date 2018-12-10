22dc48c7373ec9ffd76319a985bce7c88ecd92a5
unknown <watercolours@qq.com>
2018-12-10 08:39:59 +0800

    performence

--- Travel/src/pages/city/components/Alphabet.vue
+++ Travel/src/pages/city/components/Alphabet.vue
@@ -1,6 +1,8 @@
 <template>
         <ul class="list">
-            <li class="item" v-for="item of letters" :key="item"
+            <li class="item"
+                v-for="item of letters"
+                :key="item"
                 @touchstart="handleTouchStart"
                 @touchmove="handleTouchMove"
                 @touchend="handleTouchEnd"
@@ -30,28 +32,38 @@ export default{
      },
      data (){
         return {
-            touchStatus: false
+            touchStatus: false,
+            startY: 0,
+            timer: null
         }
+     },
+     // 当页面数据被更新且页面被渲染后会执行
+     updated (){
+         // 获取a元素距离顶部高度
+        this.startY = this.$refs['A'][0].offsetTop
      },
      methods: {
         handleLetterClick (e){
             this.$emit('change',e.target.innerText)
-
         },
         handleTouchStart (){
             this.touchStatus = true
         },
         handleTouchMove (e){
-            if(this.touchStatus){
-                // 获取a元素距离顶部高度
-                const startY =this.$refs['A'][0].offsetTop
-                const touchY=e.touches[0].clientY - 79
-                const index = Math.floor((touchY - startY) / 20)
-                if(index >= 0 && index<= this.letters.length)
-                {
-                    this.$emit('change',this.letters[index])
-                }
 
+            if(this.touchStatus){
+                //时间节流
+                if(this.timer){
+                    clearTimeout(this.timer)
+                }
+                this.timer = setTimeout(() => {
+                    const touchY=e.touches[0].clientY - 79
+                    const index = Math.floor((touchY - this.startY) / 20)
+                    if(index >= 0 && index<= this.letters.length)
+                    {
+                        this.$emit('change',this.letters[index])
+                    }
+                },16)
             }
         },
         handleTouchEnd (){
@@ -67,7 +79,6 @@ export default{
         display: flex
         flex-direction: column
         justify-content: center
-
         position: absolute
         top: 1.58rem
         right: 0
